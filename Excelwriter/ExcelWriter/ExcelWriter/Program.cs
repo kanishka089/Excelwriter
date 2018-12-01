75e81bc2680ba6f4a1f40043a9a462d38ca86ab0
Yohan <kanishka.gunawardana@gmail.com>
2018-12-01 09:46:02 +0530

    first

---
+++ Excelwriter/ExcelWriter/ExcelWriter/Program.cs
@@ -0,0 +1,57 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                excel.Workbook.Worksheets.Add("Worksheet1");
+                var headerRow = new List<string[]>()
+                {
+                    new string[] { "ID", "First Name", "Last Name", "DOB" }
+                };
+                // Determine the header range (e.g. A1:D1)
+                string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
+                // Target a worksheet
+                var worksheet = excel.Workbook.Worksheets["Worksheet1"];
+                // Popular header row data
+                worksheet.Cells[headerRange].LoadFromArrays(headerRow);
+                //------------------------------------------write data------------------------------------------------------------
+                DataTable dt = new DataTable();
+                dt.Clear();
+                dt.Columns.Add("ID");
+                dt.Columns.Add("Fname");
+                dt.Columns.Add("Lname");
+                dt.Columns.Add("Dob");
+                DataRow _ravi = dt.NewRow();
+                _ravi["ID"] = "001";
+                _ravi["Fname"] = "Kani";
+                _ravi["Lname"] = "Gun";
+                _ravi["Dob"] = "500";
+                DataRow _ravi1 = dt.NewRow();
+                _ravi1["ID"] = "002";
+                _ravi1["Fname"] = "Yhn";
+                _ravi1["Lname"] = "ravi";
+                _ravi1["Dob"] = "600";
+                dt.Rows.Add(_ravi);
+                dt.Rows.Add(_ravi1);
+                worksheet.Cells.LoadFromDataTable(dt,true);
+                //------------------------------------------------------------------------------------------------
+                FileInfo excelFile = new FileInfo(@"C:\test\test.xlsx");
+                excel.SaveAs(excelFile);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Excelwriter/ExcelWriter/ExcelWriter/emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelWriter
+{
+    public class emp
+    {
+        public string ID { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Dob { get; set; }
+    }
+}
